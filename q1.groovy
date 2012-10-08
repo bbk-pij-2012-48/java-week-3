f51f7d5f24d9bfb36a961813dc82361727b8193d
Richard Bennett <rbenne06@dcs-bbk-ac-uk>
2012-10-08 19:56:09 +0100

    Initial commit - only takes input and displays menu at this point

---
+++ q1.groovy
@@ -0,0 +1,6 @@
+print "Enter two numbers: ";
+double number1 = Double.parseDouble(System.console().readLine());
+double number2 = Double.parseDouble(System.console().readLine());
+
+println "What would you like to do with your numbers?";
+println "1. Add \n2. Subtract \n3. Multiply \n4. Divide\n";
