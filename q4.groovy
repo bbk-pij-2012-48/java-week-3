a676a0d4bef2ca3f5aa0b043ae339a3e80ef3160
Richard Bennett <rbennett485-gmail-com>
2012-10-14 20:46:37 +0100

    For loop

--- q4.groovy
+++ q4.groovy
@@ -1,8 +1,6 @@
 print "Enter sentence: ";
 String str = System.console().readLine();
-int counter = 0;
 
-while (counter < str.length()){
-      println str.charAt(counter);
-      counter++;
+for (int a=0 ; a<str.length() ; a++){
+    println str.charAt(a);
 }
