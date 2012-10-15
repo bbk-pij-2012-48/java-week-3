0bfb2e96e7aeeedd276a56ab6dfda18a1d26fa29
Richard Bennett <rbennett485-gmail-com>
2012-10-15 10:27:20 +0100

    writes each word on new line

---
+++ q4b.groovy
@@ -0,0 +1,11 @@
+print "Enter sentence: ";
+String str = System.console().readLine();
+
+for(int a=0 ; a<str.length() ; a++){
+        if(str.charAt(a)==' ')
+                            println "";
+        else
+                            print str.charAt(a);
+}
+
+println "";
