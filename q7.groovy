dea64d53eb9a31b066d15812ec8567ce0fa90486
Richard Bennett <rbenne06@dcs-bbk-ac-uk>
2012-10-15 18:26:58 +0100

    Q7 now terminates if any character is repeated, not only if it is repeated twice in a row

--- q7.groovy
+++ q7.groovy
@@ -1,13 +1,15 @@
 print "Enter sentence: ";
 String str = System.console().readLine();
-char newTargetChar = 'a', oldTargetChar = '\u0000'; //null char
-int targetCount = 0;
+char newTargetChar = 'a';
+char[] oldTargetChar = new char[26];
+int targetCount = 0, searchCount = 0;
+boolean doubled = false;
 
 print "Enter character to search for: ";
 newTargetChar = System.console().readLine();
 newTargetChar = Character.toLowerCase(newTargetChar);
 
-while(oldTargetChar!=newTargetChar){
+while(!doubled){
 	targetCount=0; 
 
 	for(int a=0 ; a<str.length() ; a++){
@@ -17,10 +19,20 @@ while(oldTargetChar!=newTargetChar){
 
 	println "There are " + targetCount + " '" + newTargetChar + "'s in the text";
 	
-	oldTargetChar = newTargetChar;
+	oldTargetChar[searchCount] = newTargetChar;
+	
 	print "Enter character to search for: ";
 	newTargetChar = System.console().readLine();
-	newTargetChar = Character.toLowerCase(newTargetChar);
+	newTargetChar = Character.toLowerCase(newTargetChar)
+	
+	for(int a=0 ; a<=targetCount ; a++){
+		if(newTargetChar==oldTargetChar[a]){
+			doubled = true;
+			break;
+		}
+	}
+
+	searchCount++;
 }
 
-println "Same character entered twice in a row, exiting program...";
+println "Same character entered twice, exiting program...";
